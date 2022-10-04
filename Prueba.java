c063fd710fdba898b53cb137455e9e0ebba48f11
Nicolas <nicoaldopaez@gmail.com>
2022-10-04 11:04:21 +0200

    segunda linea con edad y fecha de nacimiento

--- Prueba.java
+++ Prueba.java
@@ -3,7 +3,7 @@ public class  Prueba {
     public static void main(String args[]) {
 
         System.out.println("Paez Amarilla");
-        
+        System.out.println("28 años 1994");
 
     }
 }
