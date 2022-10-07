cdfb54413e883851708d6a7a817ce2fab25b424b
Nicolas <nicoaldopaez@gmail.com>
2022-10-07 12:21:23 +0200

    Prueba2

--- Prueba.java
+++ Prueba.java
@@ -2,8 +2,9 @@ public class  Prueba {
 
     public static void main(String args[]) {
 
-        System.out.println("Paez Amarilla");
+        
         System.out.println("28 años 1994");
 	System.out.println("Barbacoa");
+	System.out.println("Metallica");
     }
 }
